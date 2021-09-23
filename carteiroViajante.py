420e0085e1f1947cb02bf3c3e1583151bc42a214
Thiago Andrade <01tpaa.br@gmail.com>
2021-09-23 18:46:32 -0300

    precisa melhorar os pesos

--- carteiroViajante.py
+++ carteiroViajante.py
@@ -65,12 +65,11 @@ def proxPasso(matrizDist, matrizFeromonios, verticeAtual, verticesJaPassados, a,
             verticeEscolhido = listaVertices[i]
             return verticeEscolhido
 
-# proxPasso(matrizInput, matrizFeromonios, 3, verticesJaPassados, 1, 1)
 
 #Agora uma função para simular o caminho que uma formiga irá fazer
 #ou seja iteração de "passos" da função anterior até ela ter que voltar para o seu inicio
 
-#Peso padrão das arestas será "1", tenderá para escolha de arestas menores
+#Peso padrão das arestas será "1", tenderá para escolha de arestas menores se o peso dos feromonios também for igual a 1
 def caminho(matrizDist, matrizFeromonios, verticeInicial, pesoFeromonios):
     #Ela deverá escolher vertices até todos os vertices serem escolhidos
     verticesRestantes = len(matrizDist)
@@ -87,7 +86,7 @@ def caminho(matrizDist, matrizFeromonios, verticeInicial, pesoFeromonios):
     
     while(verticesRestantes != 1):
         verticeAntigo = verticeAtual
-        verticeAtual = proxPasso(matrizDist, matrizFeromonios, verticeAtual, verticesJaPassados, 1, pesoFeromonios)
+        verticeAtual = proxPasso(matrizDist, matrizFeromonios, verticeAtual, verticesJaPassados, 0.1, pesoFeromonios)
         
         #Atualizar vertices passados
         caminho.append(verticeAtual)
@@ -111,4 +110,42 @@ def caminho(matrizDist, matrizFeromonios, verticeInicial, pesoFeromonios):
     return matrizFeromonios
 
 
-caminho(matrizInput, matrizFeromonios, 0, 1.5)
+# caminho(matrizInput, matrizFeromonios, 0, 1.5)
+
+#Agora uma função para fazer a iteração de n formigas, ou seja aplicação n vezes da função anterior
+#para tentar obter um caminho bom
+#Após as n iterações, uma ultima formiga elegerá qual o caminho final baseado apenas na quantidade de feromonios presente nele
+def colonia(matrizDist, matrizFeromoniosInicial, verticeInicial, quantidadeDeFormigas, pesoFeromonios):
+    matrizFeromoniosAtual = matrizFeromoniosInicial
+    for i in range(quantidadeDeFormigas):
+        matrizFeromoniosAtual = caminho(matrizDist, matrizFeromoniosAtual, verticeInicial, pesoFeromonios)
+    
+    verticeAtual = verticeInicial
+    caminhoFinal = []
+    distCaminhoFinal = 0
+
+    for i in range(len(matrizDist)):
+        #Decidindo melhor vizinho
+        melhorVizinho = matrizFeromonios[verticeAtual][verticeAtual]
+        proxVertice = verticeAtual
+        for j in range(len(matrizDist[verticeAtual])):
+            arestaAtual = matrizFeromonios[verticeAtual][j]
+            if(arestaAtual > melhorVizinho):
+                melhorVizinho = arestaAtual
+                proxVertice = j
+        
+        if i < len(matrizDist) - 1:
+            distCaminhoFinal += matrizDist[verticeAtual][proxVertice]
+            verticeAtual = proxVertice
+            caminhoFinal.append(proxVertice)     
+        else:
+            caminhoFinal.append(verticeInicial)
+            distCaminhoFinal += matrizDist[verticeAtual][verticeInicial]
+    
+    print(caminhoFinal)
+    print(distCaminhoFinal)
+    
+
+colonia(matrizInput, matrizFeromonios, 0, 10000, 2)
+
+#Com 10000 iterações e os pesos apresentados, as formigas tendem ao caminho [5, 4, 3, 1, 2, 0] com distancia 44.0
